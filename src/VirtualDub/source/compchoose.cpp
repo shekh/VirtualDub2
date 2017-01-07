16d39fc7d9353413fe9770c8151987a7848a6936
shekhjkl <shekh.anton@gmail.com>
2017-01-08 01:01:25 +0200

    fix codec settings

--- src/VirtualDub/source/compchoose.cpp
+++ src/VirtualDub/source/compchoose.cpp
@@ -252,12 +252,10 @@ bool VDUIDialogChooseVideoCompressorW32::OnCommand(uint32 id, uint32 extcode) {
 			if (mhCodec) {
 				mhCodec->configure(mhdlg);
 				mCodecState.clear();
-				if (mhCodec->obj) {
-					int len = mhCodec->getStateSize();
-					if (len > 0) {
-						mCodecState.resize(len);
-						mhCodec->getState(mCodecState.data(), len);
-					}
+				int len = mhCodec->getStateSize();
+				if (len > 0) {
+					mCodecState.resize(len);
+					mhCodec->getState(mCodecState.data(), len);
 				}
 
 				if (mpCurrent)
